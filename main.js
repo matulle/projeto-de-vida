ae873fe4f8d02bbfa137bb4c5412c424387d7f99
matulle <137783897+matulle@users.noreply.github.com>
2025-06-06 09:06:53 -0300

    ok

--- main.js
+++ main.js
@@ -13,3 +13,24 @@ botoes[i].onclick = function () {
     textos[j].classList.remove("ativo");
 }
 }
+
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2025-12-24T00:00:00");
+
+
+contadores[0].textContent = calculaTempo(tempoObjetivo);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = match.floor(tempoFinal/1000);
+  let minutos = match.floor(segundos/60);
+  let horas = match.floor(minutos/60)
+  let dias = match.floor(horas/60)
+
+  segundos %= 60;
+  minutos %=60;
+  horas %= 24;
+
+  return dias + "dias" + horas + "horas"  + minutos + "minutos" + segundos +"segundos";
+}
